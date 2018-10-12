e327874e8801ade341efffeb7c9ef604ee801ffc
MrJiao <361606225@qq.com>
2018-10-12 13:15:52 +0800

    none

--- common-api/src/main/java/com/jackson/common/api/function/InputManager.java
+++ common-api/src/main/java/com/jackson/common/api/function/InputManager.java
@@ -1,6 +1,9 @@
 package com.jackson.common.api.function;
 
 
+import com.jackson.common.api.utils.L;
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.Scanner;
 
 /**
@@ -8,13 +11,42 @@ import java.util.Scanner;
  */
 public class InputManager {
 
-    public String getInput(){
-        String read;
+    public static String getInput(String ... chooseStr) {
         Scanner scan = new Scanner(System.in);
-        while ((read = scan.nextLine()) != null) {
-            return read;
+        String read = null;
+        while (true) {
+            read = scan.nextLine();
+            if (StringUtils.isEmpty(read))
+                continue;
+
+            if(!isMatch(read,chooseStr)){
+                printMust(chooseStr);
+                continue;
+            }
+            break;
         }
-        return null;
+        return read;
+    }
+
+    private static boolean isMatch(String input,String...msg){
+        boolean match = false;
+        if(msg==null || msg.length==0)return true;
+        for (String s : msg) {
+            if(StringUtils.equalsIgnoreCase(input,s)){
+                match=true;
+                break;
+            }
+        }
+        return match;
+    }
+
+
+    private static void printMust(String... msg){
+        String temp="";
+        for (String s : msg) {
+            temp +=s+" ";
+        }
+        L.d("你输错啦，只能输入",temp);
     }
 
 }
